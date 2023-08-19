56a18570b7dc8f0a737d268cfa8e4d9fc274c67b
babujee80 <139690594+babujee80@users.noreply.github.com>
2023-08-19 02:03:46 -0700

    These files uploaded from PC

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+var namelowercase = `eric`;
+var nameupercase = `ERIC`;
+var nametitlecase = `Eric`;
+console.log(namelowercase, nameupercase, nametitlecase);
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q3 name cases: Store a persons name in a variable, and ten print that person's name in lower case uppercase and titlecase. 
+
+var namelowercase = `eric`;
+var nameupercase = `ERIC`;
+var nametitlecase = `Eric`;
+console.log(namelowercase, nameupercase, nametitlecase);
